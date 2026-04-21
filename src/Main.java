1096a99044a5fcc1c5c4bdd223272a4a0c488684
Rithika-K2032 <Rithika1736@gmail.com>
2026-04-21 15:45:20 +0530

    Fourth Usecase

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
     public static void main(String[] args) {
-        int slot = 7;
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        System.out.println(isValidMove(1, 1));
     }
-    static int getRowFromSlot(int slot) {
-        if (slot < 1 || slot > 9) {
-            throw new IllegalArgumentException("Slot must be between 1 and 9");
+    static boolean isValidMove(int row, int col) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
         }
-        return (slot - 1) / 3;
-    }
-    static int getColFromSlot(int slot) {
-        if (slot < 1 || slot > 9) {
-            throw new IllegalArgumentException("Slot must be between 1 and 9");
+        if (board[row][col] != '-') {
+            return false;
         }
-        return (slot - 1) % 3;
+
+        return true;
     }
 }
